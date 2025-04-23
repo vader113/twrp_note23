a1a5b44391c78a90f4812b5d1a3730c25d2ca369
SaqibJ13 <javaidsaqib82@gmail.com>
2025-04-23 12:21:24 +0000

    added files

---
+++ README.md
@@ -0,0 +1,10 @@
+# Android device tree for VGO_TEL NOTE 23 (NOTE_23)
+
+```
+#
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```
---
+++ omni_NOTE_23.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from NOTE_23 device
+$(call inherit-product, device/ssh/NOTE_23/device.mk)
+
+PRODUCT_DEVICE := NOTE_23
+PRODUCT_NAME := omni_NOTE_23
+PRODUCT_BRAND := VGO_TEL
+PRODUCT_MODEL := NOTE 23
+PRODUCT_MANUFACTURER := ssh
+
+PRODUCT_GMS_CLIENTID_BASE := android-ssh
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="NOTE_23_V02-user 12 SP1A.210812.016 1704769981 release-keys"
+
+BUILD_FINGERPRINT := VGO_TEL/NOTE_23_V02/NOTE_23:13/TP1A.220624.014/1704769981:user/release-keys
